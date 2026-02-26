fca4b4cb9b76331a7df9ba07c49d7b5c52d6c770
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-26 14:54:20 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,34 @@
 
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+const bcrypt = require('bcrypt');
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const result = await pool.query(
+    "SELECT * FROM UserInformation WHERE email=$1",
+    [email]
+  );
+
+  if (result.rows.length === 0) return res.send("User not found");
+
+  const user = result.rows[0];
+
+  if (!user.isactive) return res.send("User inactive");
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) return res.send("Invalid password");
+
+  req.session.user = user;
+
+  if (user.role === "Admin") {
+    res.redirect('/admin.html');
+  } else {
+    res.redirect('/dashboard.html');
+  }
+});
+
+module.exports = router;
