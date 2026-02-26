8422799c9753667a9f12c0d59352acd10685ee78
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-26 14:57:19 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,14 @@
 
+router.post('/create-student', async (req, res) => {
+  const { name, email, mobile, password, sessionid } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+
+  await pool.query(
+    `INSERT INTO UserInformation
+     (name,email,mobile,password,role,isactive,sessionid)
+     VALUES($1,$2,$3,$4,'Student',true,$5)`,
+    [name,email,mobile,hashed,sessionid]
+  );
+
+  res.redirect('/admin.html');
+});
